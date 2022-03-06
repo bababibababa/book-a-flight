4be85187199573ded7124d831d73776d1d3dce00
Supatchaya <89146187+bababibababa@users.noreply.github.com>
2022-03-06 20:54:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/flightbooking'));
+app.use(express.static('./dist/book_a_flight'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/flightbooking/' }),
+    res.sendFile('index.html', { root: 'dist/book_a_flight/' }),
 );
 
 app.listen(process.env.PORT || 8080);
