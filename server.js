399ef47ab878ca24bd362b0d957464f7f86bdabd
Supatchaya <89146187+bababibababa@users.noreply.github.com>
2022-03-06 21:07:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-app.use(express.static('./dist/book_a_flight'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/book_a_flight/' }),
-);
-
+app.use(express.static(__dirname + '/book_a_flight'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'book_a_flight', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
